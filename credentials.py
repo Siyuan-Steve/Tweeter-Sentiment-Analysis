32ac529e129695ff5d690ff7d9301590be90b76a
siyuan Li <sl4072@columbia.edu>
2017-10-31 18:46:58 -0400

    added credentials.py

---
+++ credentials.py
@@ -0,0 +1,9 @@
+# Twitter App access keys for @user
+
+# Consume:
+CONSUMER_KEY    = 'CaIQqfXoZUABREB7v1KGQ5SC9'
+CONSUMER_SECRET = '6SKbKEvgu0zmM6nRyw9fv4D0bkXtRERv7FMNcQzbRmhuFuhKWU'
+
+# Access:
+ACCESS_TOKEN  = '719149654277902336-W4roeKJbrmq2dulInVctuo7qYuhBnOv'
+ACCESS_SECRET = 'EDZoIz3JVJJ5Skmzk21FVTBGpGgbXGGOgnHB0j8ljUvAJ'
